98141c6451f8e9d2f17e65367d6316ae20486af5
usbbush <jeanbienvenusb@.com>
2025-04-24 17:54:40 -0400

    Rename Procfiledev to Procfile

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,8 @@
-require "redis"
-if Rails.env.test?
-  require "mock_redis"
-  REDIS = MockRedis.new
-elsif Rails.env.production?
-  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
-else
-  redis_config = { url: ENV["REDIS_URL"] || "redis://localhost:6379/0" }
-  REDIS = Redis.new(redis_config)
+url = ENV["REDISCLOUD_URL"] || ENV["REDIS_URL"]
+Sidekiq.configure_server do |config|
+  config.redis = { url: url }
 end
+Sidekiq.configure_client do |config|
+  config.redis = { url: url }
+end
+$redis = Redis.new(url: url)
